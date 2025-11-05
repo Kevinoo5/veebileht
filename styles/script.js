6988fafbb6ccd41bf2cc85ddff860672184979fc
Marcus Adamson <56176889+marcusadamson123@users.noreply.github.com>
2025-11-05 19:46:06 +0200

    Avalehe script

--- styles/script.js
+++ styles/script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const menuIcon = document.getElementById("menuIcon");
+  const navLinks = document.getElementById("navLinks");
+
+  menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
 
