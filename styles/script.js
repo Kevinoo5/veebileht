0e3231f0444d18b872c511f38dc59fb8ef716463
Marcus Adamson <56176889+marcusadamson123@users.noreply.github.com>
2025-11-30 16:10:39 +0200

    kommentaarid

--- styles/script.js
+++ styles/script.js
@@ -1,13 +1,14 @@
+//Script hakkab töötama siis, kui leht on täielikult laetud
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menuIcon");
   const navLinks = document.getElementById("navLinks");
-
+// Vajutamisel muutub menüü nähtavaks või peidetuks olenevalt kas on active klass olemas või mitte.
   if (menuIcon && navLinks) {
     menuIcon.addEventListener("click", () => {
       navLinks.classList.toggle("active");
     });
   }
-
+// Võimalikud review'd.
   const reviews = [
     {
       text: "Ostsin Couch Havenist diivani ja olen väga rahul! Väga mugav ja stiilne.",
@@ -27,16 +28,18 @@ document.addEventListener("DOMContentLoaded", function () {
       author: "Marcus A.",
     },
   ];
-
+/* Constant hoiab meeles, milline review on hetkel nähtaval.
+const-id viitavad HTML elementidele, kus review tekst ja autor kuvatakse.
+*/
   let current = 0;
   const textEl = document.getElementById("review-text");
   const authorEl = document.getElementById("review-author");
   const reviewBox = document.querySelector(".review-box");
-
+// Kui kõik on leitud hakkavad review'd vahetuma iga 6 sekundi tagant haistusega.
   if (textEl && authorEl && reviewBox) {
     function nextReview() {
       reviewBox.style.opacity = 0;
-
+ //Review lõpetamine ja järgmise kuvamine, current muutub järgmise review indeksiks ja tekstid, autorid vahetuvad.
       setTimeout(() => {
         current = (current + 1) % reviews.length;
         textEl.textContent = `"${reviews[current].text}"`;
